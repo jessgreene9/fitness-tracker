386fd88d7468a674b624b4e0bc62ef575fda294b
Jess Greene <jess.greene9@gmail.com>
2021-07-09 09:44:26 -0400

    adding aggregate and catches

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,14 +2,28 @@ const db = require("../models");
 const router = require("express").Router();
 
 router.get("/api/workouts", (req, res) => {
-  db.Workout.find({}).then((workouts) => {
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ]).then((workouts) => {
     res.json(workouts);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.json(err);
   });
 });
 
 router.post("/api/workouts", (req, res) => {
   db.Workout.create(req.body).then((workout) => {
     res.json(workout);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.json(err);
   });
 });
 
@@ -19,12 +33,20 @@ router.put("/api/workouts/:id", (req, res) => {
     { $push: { exercises: req.body } }
   ).then((workout) => {
     res.json(workout);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.json(err);
   });
 });
 
 router.get("/api/workouts/range", (req, res) => {
   db.Workout.find({}).then((workouts) => {
     res.json(workouts);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.json(err);
   });
 });
 
